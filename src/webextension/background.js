6609455d055fc98866ad6b18233c8534f92a88df
Chuck Harmston <chuck@harmston.ch>
2017-01-03 11:29:17 -0700

    Load pageAction into existing tabs on install.

--- src/webextension/background.js
+++ src/webextension/background.js
@@ -3,11 +3,21 @@ const { Logger } = require('../lib/log');
 const logger = new Logger('background', console);
 
 
-browser.pageAction.onClicked.addListener(function () {
-  logger.log('pageAction created');
+// Show the page action for all existing tabs.
+browser.tabs.query({}).then(tabs => {
+  for (tab of tabs) {
+    logger.log('Loaded pageAction into existing tab', tab.id);
+    browser.pageAction.show(tab.id);
+  }
 });
 
+// Show the page action on new tabs.
 browser.tabs.onCreated.addListener(tab => {
-  logger.log(`tab created ${tab.id}`);
+  logger.log('Loaded pageAction into new tab', tab.id);
   browser.pageAction.show(tab.id);
+});
+
+// Handle clicks on the pageAction icon.
+browser.pageAction.onClicked.addListener(() => {
+  logger.log('Click on pageAction');
 });
