dff661728378b579dd56a0a10ec6ce851e481220
Chuck Harmston <chuck@harmston.ch>
2017-01-18 23:58:37 -0700

    Adds prettier command; runs it on working tree.

--- src/webextension/survey/survey.js
+++ src/webextension/survey/survey.js
@@ -1,20 +1,17 @@
-import styles from './styles.scss';
-import Logger from '../../lib/log';
-import Open from './lib/open';
+import styles from "./styles.scss";
+import Logger from "../../lib/log";
+import Open from "./lib/open";
 
-const logger = new Logger('webext.survey', window.console);
-
+const logger = new Logger("webext.survey", window.console);
 
 function main() {
-  logger.log('Initializing');
+  logger.log("Initializing");
 
-  window.app = {
-    open: new Open()
-  };
+  window.app = { open: new Open() };
 }
 
-if (document.readyState !== 'loading'){
+if (document.readyState !== "loading") {
   main();
 } else {
-  document.addEventListener('DOMContentLoaded', main);
+  document.addEventListener("DOMContentLoaded", main);
 }
