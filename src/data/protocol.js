fa564fb8c7b6555ea4d94efd2db57d2b8a9730ea
Chuck Harmston <chuck@harmston.ch>
2017-01-30 15:41:35 -0700

    Include protocol in reports (closes #35).

---
+++ src/data/protocol.js
@@ -0,0 +1,1 @@
+self.port.emit('protocol', window.location.protocol);
--- src/measurements/index.js
+++ src/measurements/index.js
@@ -1,15 +1,15 @@
 import { storage } from 'sdk/simple-storage';
 import { getMostRecentBrowserWindow } from 'sdk/window/utils';
+import Logger from '../lib/log';
 
+import Protocol from './protocol';
 import TelemetryId from './telemetry-id';
 
-import Logger from '../lib/log';
+const MEASUREMENTS = [ Protocol, TelemetryId ];
 const logger = new Logger(
   'sdk.measurements',
   getMostRecentBrowserWindow().console
 );
-
-const MEASUREMENTS = [ TelemetryId ];
 
 // Passed the output from the survey, augments that data with each measurment
 // in MEASUREMENTS and returns a promise resolving to a Map containing the full
