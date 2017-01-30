b21584cbeca33ee3abd37e983d9e524a584e425f
Chuck Harmston <chuck@harmston.ch>
2017-01-30 16:12:50 -0700

    Includes number of open tabs in reports (closes #27).

--- src/measurements/index.js
+++ src/measurements/index.js
@@ -1,15 +1,17 @@
 import { storage } from 'sdk/simple-storage';
 import { getMostRecentBrowserWindow } from 'sdk/window/utils';
 
+import Logger from '../lib/log';
+
+import OpenTabs from './open-tabs';
 import TelemetryId from './telemetry-id';
 
-import Logger from '../lib/log';
 const logger = new Logger(
   'sdk.measurements',
   getMostRecentBrowserWindow().console
 );
 
-const MEASUREMENTS = [ TelemetryId ];
+const MEASUREMENTS = [ OpenTabs, TelemetryId ];
 
 // Passed the output from the survey, augments that data with each measurment
 // in MEASUREMENTS and returns a promise resolving to a Map containing the full
