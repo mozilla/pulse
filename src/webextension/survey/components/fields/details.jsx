cd35144e867890fbe7efd12088e7a3f2684ea841
Chuck Harmston <chuck@mozilla.com>
2017-04-05 11:47:13 -0600

    Revert "Limit details field to 4096 characters (closes #140)."
    This reverts commit da56f3b182ac07d00635ac4ec8938d55b5f2a91f.

--- src/webextension/survey/components/fields/details.jsx
+++ src/webextension/survey/components/fields/details.jsx
@@ -1,35 +1,16 @@
-import classNames from 'classnames';
 import React, { Component } from 'react';
 
 export default class DetailsField extends Component {
-  renderError(error) {
-    if (error) {
-      return <div className="field--error-message">{error}</div>;
-    }
-    return null;
-  }
-
-  getClassNames() {
-    const { meta: { error } } = this.props;
-    return classNames('field', 'field--details', { 'field--error': !!error });
-  }
-
   render() {
-    const { input, meta: { error } } = this.props;
+    const { input } = this.props;
     return (
-      <div className={this.getClassNames()}>
+      <div className="field field--details">
         <label className="step" htmlFor={input.name}>3.</label>
         <label htmlFor={input.name} id={input.name}>Any other comments?</label>
         <textarea placeholder="Optional comments..." {...input} />
-        {this.renderError(error)}
       </div>
     );
   }
 }
 
-DetailsField.propTypes = {
-  input: React.PropTypes.object.isRequired,
-  meta: React.PropTypes.shape({
-    error: React.PropTypes.string
-  })
-};
+DetailsField.propTypes = { input: React.PropTypes.object.isRequired };
